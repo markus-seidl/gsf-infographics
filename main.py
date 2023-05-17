1ab5fd1a2e5c5097952c75b5dd647c5773f53426
Markus Seidl <markus.seidl@nttdata.com>
2023-05-17 11:30:44 +0200

    Last pyCairo version.

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 import math
+import copy
 from collections import namedtuple
 
 import cairo
 
-WIDTH, HEIGHT = 1024, 1024
-CO2_SIZE = [10, 10]
-
 Point = namedtuple('Point', 'x y')
+
+WIDTH, HEIGHT = 1024, 1024
+CO2_SIZE = Point(10, 10)
+CO2_SPACING_1 = Point(3, 3)
+CO2_SPACING_2 = Point(0, 0)
+
+
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(
+            self.x + other.x,
+            self.y + other.y
+        )
 
 
 # https://pycairo.readthedocs.io/en/latest/reference/index.html
 
-def draw_co2_block(c: cairo.Context, size: Point, coords: Point):
-    pass
+def draw_co2_block(c: cairo.Context, position: Point, count: Point):
+    co2_pos = copy.copy(position)
+
+    for x in range(0, count.x):
+        for y in range(0, count.y):
+            if y % 6 == 5:
+                co2_pos.y += CO2_SPACING_2.y
+            else:
+                co2_pos.y += CO2_SPACING_1.y
+
+            draw_co2_single(c, co2_pos)
+            co2_pos.y += CO2_SIZE.y
+
+        if x % 6 == 5:
+            co2_pos.x += CO2_SPACING_2.x
+        else:
+            co2_pos.x += CO2_SPACING_1.x
+        co2_pos.x += CO2_SIZE.x
+        co2_pos.y = position.y
 
 
 def draw_co2_single(c: cairo.Context, coords: Point = None):
@@ -22,7 +54,7 @@ def draw_co2_single(c: cairo.Context, coords: Point = None):
     c.set_source_rgb(0.0, 0.8, 0.1)  # Solid color
 
     c.set_line_width(2)
-    c.rectangle(coords.x, coords.y, CO2_SIZE[0], CO2_SIZE[1])
+    c.rectangle(coords.x, coords.y, CO2_SIZE.x, CO2_SIZE.y)
     c.fill_preserve()
     c.stroke()
 
@@ -32,7 +64,7 @@ if __name__ == "__main__":
     surface = cairo.SVGSurface("example.svg", WIDTH, HEIGHT)
     ctx = cairo.Context(surface)
 
-    draw_co2_single(ctx)
+    draw_co2_block(ctx, Point(0, 0), Point(10, 5))
 
 # pat = cairo.LinearGradient(0.0, 0.0, 0.0, 1.0)
 # pat.add_color_stop_rgba(1, 0.7, 0, 0, 0.5)  # First stop, 50% opacity
